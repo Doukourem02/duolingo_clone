4177f31c196203883ef265a9d89f90a3cd1cc947
Doukourem02 <mohameddoukoure340@gmail.com>
2026-08-05 03:10:51 +0000

    Remove duplicate Baro/backend copies inside nextjs-duolingo-clone

--- nextjs-duolingo-clone/backend/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { serve } from "@hono/node-server";
-
-import app from "./app";
-import { env } from "./env";
-
-serve({ fetch: app.fetch, port: env.PORT }, (info) => {
-  console.log(`backend listening on http://localhost:${info.port}`);
-});
